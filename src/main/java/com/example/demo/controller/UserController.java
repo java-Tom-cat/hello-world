7fcb8df8b61ccb53b577a1db25908e1dc3d56950
java-Tom-cat <Administrator@192.168.0.100>
2019-10-13 12:29:50 +0800

    test2

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
@@ -19,12 +21,15 @@ public class UserController {
 	@Autowired
 	private UserMapper userMapper;
 
-	@RequestMapping("/user")
-	public String select(HttpSession session) {
+	@RequestMapping("/test")//test为jsp文件名
+	public ModelAndView select(HttpSession session,HttpServletRequest rep) {
 		List<User> list=userMapper.queryAll();
 		session.setAttribute("list",list);
-		 return userMapper.queryAll().toString();
-		//return "ok";
+		//跳转jsp页面
+		// return userMapper.queryAll().toString();
+		 ModelAndView mView=new ModelAndView();
+		
+		return mView;
 	}
 	
 	 @Autowired
